d2048f3718df7c3c46d2485cea90668234b0e7e5
simonas <simonas.pacauskas@gmail.com>
2024-04-16 16:49:46 +0300

    ping pong

--- src/Server.cs
+++ src/Server.cs
@@ -1,8 +1,33 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 Console.WriteLine("Logs from your program will appear here!");
 
 var server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
-server.AcceptSocket(); // wait for client
+
+while (true)
+{
+    server.BeginAcceptSocket(ConnectionCallback, server);
+}
+
+void ConnectionCallback(IAsyncResult asyncResult)
+{
+    var socket = server.EndAcceptSocket(asyncResult);
+    Console.WriteLine($"TCP Connection [{socket.LocalEndPoint}->{socket.RemoteEndPoint}] established!");
+
+    var buffer = new StringBuilder();
+    var tempBuffer = new byte[1024];
+    int bytesRead;
+
+    do
+    {
+        bytesRead = socket.Receive(tempBuffer);
+        buffer.Append(Encoding.ASCII.GetString(tempBuffer, 0, bytesRead));
+    } while (bytesRead == tempBuffer.Length);
+
+    var clientCommand = buffer.ToString();
+    socket.Send(Encoding.ASCII.GetBytes("PONG\r\n"));
+    socket.Close();
+}
