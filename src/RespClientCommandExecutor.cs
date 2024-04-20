7d1910af0bde878edf8058c533b87190c9b7383e
simonas <simonas.pacauskas@gmail.com>
2024-04-20 18:29:28 +0300

    test

--- src/RespClientCommandExecutor.cs
+++ src/RespClientCommandExecutor.cs
@@ -14,31 +14,43 @@ public class RespClientCommandExecutor
             _ => throw new ArgumentException("Invalid RESP client command.")
         };
     }
-    
+
     private string ExecuteSimpleString(string respCommandString)
     {
         return "+PONG\\r\\n";
     }
-    
+
     private string ExecuteSimpleError(string respCommandString)
     {
         return "-ERR unknown command 'foobar'\r\n";
     }
-    
+
     private string ExecuteInteger(string respCommandString)
     {
         return ":1000\r\n";
     }
-    
+
     private string ExecuteBulkString(string respCommandString)
     {
         return "$6\r\nfoobar\r\n";
     }
-    
+
     private string ExecuteArray(string respCommandString)
     {
         var commandParts = respCommandString.Split("\\r\\n");
+        var commandCount = int.Parse(commandParts[0].Replace("*", string.Empty));
+
+        if (commandCount == 1 && string.Equals(commandParts[2], "ping", StringComparison.InvariantCultureIgnoreCase))
+        {
+            return "+PONG\\r\\n";
+        }
         
-        return "*2\r\\n$3\\r\\nfoo\\r\\n$3\\r\\nbar\\r\\n";
+        if (commandCount == 2 &&
+                 string.Equals(commandParts[2], "ping", StringComparison.InvariantCultureIgnoreCase))
+        {
+            return $"${commandParts[4].Length}\\r\\n{commandParts[4]}\\r\\n";
+        }
+
+        return "*2\r\n$3\r\nfoo\r\n$3\r\nbar\r\n";
     }
 }
