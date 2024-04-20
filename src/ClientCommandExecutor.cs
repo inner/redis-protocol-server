a8f000d3fef76894237324baa81e257f5080efff
simonas <simonas.pacauskas@gmail.com>
2024-04-20 17:45:31 +0300

    test

---
+++ src/ClientCommandExecutor.cs
@@ -0,0 +1,53 @@
+﻿namespace codecrafters_redis;
+
+public class ClientCommandExecutor
+{
+    public string Execute(string clientCommandString)
+    {
+        clientCommandString = clientCommandString
+            .Replace("\n", "")
+            .Trim();
+        
+        var commandParts = clientCommandString
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Select(x => x.ToLower())
+            .ToArray();
+        
+        var commandType = GetClientCommandType(commandParts[0]);
+        
+        return commandType switch
+        {
+            ClientCommandType.Ping => "+PONG\r\n",
+            ClientCommandType.Echo => $"${commandParts[1].Length}\\r\\n{commandParts[1]}\\r\\n",
+            _ => throw new ArgumentException("Invalid client command.")
+        };
+    }
+
+    private ClientCommandType GetClientCommandType(string clientCommand)
+    {
+        if (string.IsNullOrWhiteSpace(clientCommand))
+        {
+            throw new ArgumentException("Invalid client command.");
+        }
+
+        if (string.Equals(clientCommand, ClientCommandType.Ping.ToString(),
+                StringComparison.InvariantCultureIgnoreCase))
+        {
+            return ClientCommandType.Ping;
+        }
+
+        if (string.Equals(clientCommand, ClientCommandType.Echo.ToString(),
+                StringComparison.InvariantCultureIgnoreCase))
+        {
+            return ClientCommandType.Echo;
+        }
+
+        throw new ArgumentException("Invalid client command.");
+    }
+}
+
+public enum ClientCommandType
+{
+    Ping,
+    Echo
+}
--- src/Server.cs
+++ src/Server.cs
@@ -8,7 +8,7 @@ Console.WriteLine("Logs from your program will appear here!");
 var server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
 
-// var respClientCommandExecutor = new RespClientCommandExecutor();
+var clientCommandExecutor = new ClientCommandExecutor();
 
 while (true)
 {
@@ -19,30 +19,31 @@ void ConnectionCallback(IAsyncResult asyncResult)
 {
     var socket = server.EndAcceptSocket(asyncResult);
     var connectionId = $"{socket.LocalEndPoint}->{socket.RemoteEndPoint}";
-    
+
     while (socket.Connected)
     {
         try
         {
             Console.WriteLine($"TCP Connection [{connectionId}] established!");
 
-            var buffer = new byte[1024];
-            var data = socket.Receive(buffer);
-
-            while (data > 0)
+            while (true)
             {
+                var buffer = new byte[1024];
+                var data = socket.Receive(buffer);
+
                 var respClientCommandString = Encoding.UTF8.GetString(buffer, 0, data);
+                
+                if (string.IsNullOrWhiteSpace(respClientCommandString))
+                {
+                    socket.Send(Encoding.UTF8.GetBytes(Environment.NewLine));
+                    continue;
+                }
+                
                 Console.WriteLine($"[{connectionId}] received: {respClientCommandString}");
 
-                var commands = respClientCommandString.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-                var response = $"${commands[1].Length}\r\n{commands[1]}\r\n";
-                
-                // var respCommandType = respClientCommandString.GetRespClientCommandType();
-                // var respResponse = respClientCommandExecutor.Execute(respCommandType, respClientCommandString);
-                // socket.Send("+PONG\r\n"u8.ToArray());
-                
+                var response = clientCommandExecutor.Execute(respClientCommandString);
+
                 socket.Send(Encoding.UTF8.GetBytes(response));
-                data = socket.Receive(buffer);
             }
         }
         catch (SocketException)
