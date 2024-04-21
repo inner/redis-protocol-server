de5a84035a9d485a8ca3813913018799b4708cea
simonas <simonas.pacauskas@gmail.com>
2024-04-21 15:07:40 +0300

    test

--- src/RespCommands/Echo.cs
+++ src/RespCommands/Echo.cs
@@ -4,6 +4,10 @@ public class Echo : RespCommandBase
 {
     public override string Execute(int commandCount, string[] commandParts)
     {
-        throw new NotImplementedException();
+        return commandCount switch
+        {
+            2 => $"${commandParts[4].Length}\r\n{commandParts[4]}\r\n",
+            _ => throw new ArgumentException("Wrong number of arguments for 'echo' command")
+        };
     }
 }
