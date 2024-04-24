b85e3c87c4d5d1f965fe5fe890ef02ff1918d876
simonas <simonas.pacauskas@gmail.com>
2024-04-24 17:06:33 +0300

    retest

--- src/Commands/Info.cs
+++ src/Commands/Info.cs
@@ -8,15 +8,15 @@ public class Info : Base
     {
         var dict = new Dictionary<string, string>
         {
-            { "role", ServerInfo.IsMaster ? "master" : "slave" },
+            // { "role", ServerInfo.IsMaster ? "master" : "slave" },
             { "master_replid", "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb" },
             { "master_repl_offset", "0" },
-            { "connected_slaves", "0" },
-            { "second_repl_offset", "-1" },
-            { "repl_backlog_active", "0" },
-            { "repl_backlog_size", "1048576" },
-            { "repl_backlog_first_byte_offset", "0" },
-            { "repl_backlog_histlen", string.Empty }
+            // { "connected_slaves", "0" },
+            // { "second_repl_offset", "-1" },
+            // { "repl_backlog_active", "0" },
+            // { "repl_backlog_size", "1048576" },
+            // { "repl_backlog_first_byte_offset", "0" },
+            // { "repl_backlog_histlen", string.Empty }
         };
 
         var sb = new StringBuilder();
