40fdd0a40412d2319e1aebaea0e153f760b519c1
simonas <simonas.pacauskas@gmail.com>
2024-04-24 17:17:56 +0300

    pwnd

--- src/Commands/Info.cs
+++ src/Commands/Info.cs
@@ -6,28 +6,22 @@ public class Info : Base
 {
     public override string Execute(int commandCount, string[] commandParts)
     {
-        var dict = new Dictionary<string, string>
+        var infoValues = new Dictionary<string, string>
         {
-            { "master_replid", "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb" },
+            { "role", ServerInfo.IsMaster ? "master" : "slave" },
+            { "master_replid", GenerateRandomReplId() },
             { "master_repl_offset", "0" },
-            { "role", ServerInfo.IsMaster ? "master" : "slave" }
-            // { "connected_slaves", "0" },
-            // { "second_repl_offset", "-1" },
-            // { "repl_backlog_active", "0" },
-            // { "repl_backlog_size", "1048576" },
-            // { "repl_backlog_first_byte_offset", "0" },
-            // { "repl_backlog_histlen", string.Empty }
+            { "connected_slaves", "0" },
+            { "second_repl_offset", "-1" },
+            { "repl_backlog_active", "0" },
+            { "repl_backlog_size", "1048576" },
+            { "repl_backlog_first_byte_offset", "0" },
+            { "repl_backlog_histlen", string.Empty }
         };
-
-        var sb = new StringBuilder();
-
-        foreach (var (key, value) in dict)
-        {
-            var valueString = $"{key}:{value}";
-            sb.Append($"${valueString.Length}\r\n{valueString}\r\n");
-        }
-
-        return sb.ToString();
+        
+        var value = string.Join('\n', infoValues.Select(x => $"{x.Key}:{x.Value}"));
+        
+        return $"${value.Length}\r\n{value}\r\n";
     }
 
     private string GenerateRandomReplId()
