9c1cbc5e4125bfa90c358bcf69b5c76baf990985
simonas <simonas.pacauskas@gmail.com>
2024-04-24 17:37:29 +0300

    retest

--- src/Server.cs
+++ src/Server.cs
@@ -94,8 +94,7 @@ void CloseSocket(string connectionId, Socket? socket)
 void LogReceivedMessage(string s, string respClientCommandString)
 {
     respClientCommandString = respClientCommandString[..^1];
-    var receivedMessage = $"[{s}] received: \"{respClientCommandString
-        .Replace("\r\n", @"\\r\\n")}\"";
+    var receivedMessage = $"[{s}] received: \"{respClientCommandString.Replace("\r\n", @"\r\n")}\"";
 
     Console.WriteLine(receivedMessage);
 }
