8bc506cd6f1abdb90e980f87c3c55de9ccdaeebe
simonas <simonas.pacauskas@gmail.com>
2024-04-28 20:18:50 +0300

    retest

--- src/Commands/Replconf.cs
+++ src/Commands/Replconf.cs
@@ -19,7 +19,10 @@ public class Replconf : Base
         if (string.Equals(commandParts[4], "getack", StringComparison.InvariantCultureIgnoreCase) &&
             string.Equals(commandParts[6], "*", StringComparison.InvariantCultureIgnoreCase))
         {
-            socket.Send(Encoding.UTF8.GetBytes("*3\r\n$8\r\nREPLCONF\r\n$3\r\nACK\r\n$1\r\n0\r\n"));
+            if (!ServerInfo.IsMaster)
+            {
+                socket.Send(Encoding.UTF8.GetBytes("*3\r\n$8\r\nREPLCONF\r\n$3\r\nACK\r\n$1\r\n0\r\n"));
+            }
         }
     }
 }
