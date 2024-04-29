76d7c0a4efc6b5ac7382a7399fc6f4f2f3ca9bbd
simonas <simonas.pacauskas@gmail.com>
2024-04-29 15:44:23 +0300

    retest

--- src/Commands/Replconf.cs
+++ src/Commands/Replconf.cs
@@ -20,28 +20,25 @@ public class Replconf : Base
         {
             socket.Send(Encoding.UTF8.GetBytes("*3\r\n$8\r\nREPLCONF\r\n$3\r\nACK\r\n$1\r\n0\r\n"));
             
-            var buffer = new byte[1024];
-            var bytesReceived = socket.Receive(buffer);
-            var response = Encoding.UTF8.GetString(buffer, 0, bytesReceived);
-            if (!response.Contains("ACK"))
-            {
-                // Handle the case where a GETACK doesn’t receive an ACK back
-                // This could be logging the error, retrying the command, etc.
-                socket.Send(Encoding.UTF8.GetBytes(Constants.NullResponse));
-            }
+            // var buffer = new byte[1024];
+            // var bytesReceived = socket.Receive(buffer);
+            // var response = Encoding.UTF8.GetString(buffer, 0, bytesReceived);
+            // if (!response.Contains("ACK"))
+            // {
+            //     // Handle the case where a GETACK doesn’t receive an ACK back
+            //     // This could be logging the error, retrying the command, etc.
+            //     socket.Send(Encoding.UTF8.GetBytes(Constants.NullResponse));
+            // }
         }
         
-        // if (!string.Equals(commandParts[4], "ack", StringComparison.InvariantCultureIgnoreCase))
-        // {
-        //     // send null
-        //     socket.Send(Encoding.UTF8.GetBytes(Constants.NullResponse));
-        // }
-        
-        // if (string.Equals(commandParts[4], "ack", StringComparison.InvariantCultureIgnoreCase) &&
-        //     string.Equals(commandParts[6], "0", StringComparison.InvariantCultureIgnoreCase))
-        // {
-        //     // send null
-        //     socket.Send(Encoding.UTF8.GetBytes(Constants.NullResponse));
-        // }
+        if (string.Equals(commandParts[4], "ack", StringComparison.InvariantCultureIgnoreCase) &&
+            string.Equals(commandParts[6], "0", StringComparison.InvariantCultureIgnoreCase))
+        {
+            socket.Send(Encoding.UTF8.GetBytes(Constants.NullResponse));
+        }
+        else
+        {
+            socket.Send(Encoding.UTF8.GetBytes(Constants.NullResponse));
+        }
     }
 }
