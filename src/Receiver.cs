9dc7e51f3f7f5b63fd03c2bdfd3395bffe8d3c08
simonas <simonas.pacauskas@gmail.com>
2024-04-29 21:21:04 +0300

    save

--- src/Receiver.cs
+++ src/Receiver.cs
@@ -40,18 +40,18 @@ public class Receiver
         var multiCommandSplit = Regex.Split(commandString, @"(\*\d+\\r\\n)")
             .Where(x => !string.IsNullOrWhiteSpace(x))
             .ToList();
-        
+
         List<string> commandsToExecute = [];
-                
+
         var skip = 0;
-        for (var i = 0; i < multiCommandSplit.Count/2; i++)
+        for (var i = 0; i < multiCommandSplit.Count / 2; i++)
         {
             var commandToExecute = string.Join(
                 string.Empty,
                 multiCommandSplit
                     .Skip(skip)
                     .Take(2));
-                    
+
             commandsToExecute.Add(commandToExecute);
             skip += 2;
         }
@@ -84,20 +84,20 @@ public class Receiver
 
         var command = (Base)Activator.CreateInstance(type)!;
 
-        if (ServerInfo.IsMaster && new[] { CommandType.Set }.Contains(commandType))
+        if (ServerInfo.IsMaster)
         {
             foreach (var replicaSocket in ServerInfo.ReplicaSockets.Where(x => x.Value.Connected))
             {
                 Console.WriteLine($"Propagating command '{commandString}' to replica '{replicaSocket.Key}'.");
-                // command.Execute(replicaSocket.Value, commandCount, commandParts, replicaConnection: true);
-                
+                command.Execute(replicaSocket.Value, commandCount, commandParts, replicaConnection: true);
+
                 replicaSocket.Value.Send(Encoding.UTF8.GetBytes(commandString.Replace("\\r\\n", "\r\n")));
             }
         }
 
         command.Execute(socket, commandCount, commandParts);
     }
-    
+
     private void ExecuteBulkString(Socket socket, string commandString)
     {
         var commandParts = commandString.Split("\\r\\n");
