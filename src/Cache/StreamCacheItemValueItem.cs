233e58f5c425a3e46283b2117229f948f00fc2ee
Simonas <simonas.pacauskas@gmail.com>
2024-07-29 18:50:21 +0300

    codecrafters submit [skip ci]

--- src/Cache/StreamCacheItemValueItem.cs
+++ src/Cache/StreamCacheItemValueItem.cs
@@ -3,7 +3,7 @@
 public class StreamCacheItemValueItem
 {
     public string Id { get; set; } = null!;
-    public string Key { get; set; }
+    public string Key { get; set; } = null!;
     public long Timestamp => long.Parse(Id.Split('-')[0]);
     public long Sequence => long.Parse(Id.Split('-')[1]);
     public List<StreamCacheItemValueItemValue> Value { get; set; } = null!;
