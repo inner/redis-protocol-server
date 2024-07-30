b370cdd6659d552a2775486ed3f05f9df42b09ff
Simonas <simonas.pacauskas@gmail.com>
2024-07-30 11:02:55 +0300

    codecrafters submit [skip ci]

---
+++ src/Commands/Incr.cs
@@ -0,0 +1,47 @@
+﻿using System.Globalization;
+using System.Net.Sockets;
+using System.Text;
+using codecrafters_redis.Cache;
+
+namespace codecrafters_redis.Commands;
+
+public class Incr : Base
+{
+    public override bool CanBePropagated => true;
+
+    protected override Task OnMasterNodeExecute(Socket socket, int commandCount, string[] commandParts,
+        bool replicaConnection = false)
+    {
+        GenerateCommonResponse(socket, commandParts, replicaConnection);
+        return Task.CompletedTask;
+    }
+
+    protected override Task OnReplicaNodeExecute(Socket socket, int commandCount, string[] commandParts,
+        bool replicaConnection = false)
+    {
+        GenerateCommonResponse(socket, commandParts, replicaConnection);
+        return Task.CompletedTask;
+    }
+
+    private static void GenerateCommonResponse(Socket socket, string[] commandParts, bool replicaConnection = false)
+    {
+        var key = commandParts[4];
+        
+        var cacheItem = DataCache.Get(key);
+        if (cacheItem == null)
+        {
+            DataCache.Set(key, "1");
+            socket.Send(Encoding.UTF8.GetBytes(":1\r\n"));
+            return;
+        }
+
+        if (!long.TryParse(cacheItem.Value, out var longValue))
+        {
+            return;
+        }
+        
+        longValue++;
+        DataCache.Set(key, longValue.ToString());
+        socket.Send(Encoding.UTF8.GetBytes($":{longValue}\r\n"));
+    }
+}
--- src/Enums/CommandTypes.cs
+++ src/Enums/CommandTypes.cs
@@ -17,5 +17,6 @@ public enum CommandTypes
     Xrange,
     Xread,
     Config,
-    Keys
+    Keys,
+    Incr
 }
