d9480138b40b40f8ac4324cb202e4ca1ed2e91a1
inner <simonas.pacauskas@gmail.com>
2024-10-22 22:06:59 +0300

    codecrafters submit [skip ci]

--- src/Commands/Keys.cs
+++ src/Commands/Keys.cs
@@ -20,8 +20,9 @@ public class Keys : Base
     {
         return await GenerateCommonResponse(socket, commandDetails, replicaConnection);
     }
-    
-    private static Task<string> GenerateCommonResponse(Socket socket, CommandDetails commandDetails, bool replicaConnection)
+
+    private static Task<string> GenerateCommonResponse(Socket socket, CommandDetails commandDetails,
+        bool replicaConnection)
     {
         string result;
         var keys = DataCache.GetKeys(commandDetails.CommandParts[4]);
@@ -30,10 +31,10 @@ public class Keys : Base
             result = "*0";
             socket.Send(Encoding.UTF8.GetBytes(result));
         }
-        
+
         var sb = new StringBuilder();
         sb.Append($"*{keys.Count}\r\n");
-        
+
         foreach (var key in keys)
         {
             sb.Append($"${key.Length}\r\n{key}\r\n");
