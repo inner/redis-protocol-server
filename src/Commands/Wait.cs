a4c40b118b14417907be00e40eaf613580328ca5
inner <simonas.pacauskas@gmail.com>
2024-11-07 15:02:19 +0200

    codecrafters submit [skip ci]

--- src/Commands/Wait.cs
+++ src/Commands/Wait.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics;
 using System.Text;
+using codecrafters_redis.Common;
 
 namespace codecrafters_redis.Commands;
 
@@ -10,24 +11,27 @@ public class Wait : Base
     protected override async Task<string> OnMasterNodeExecute(CommandContext commandContext)
     {
         ServerInfo.Replication.ReplicaAcksReceived = 0;
-        
+
         var numberOfReplicasToWaitFor = commandContext.CommandDetails.CommandParts[4];
         var msToWait = commandContext.CommandDetails.CommandParts[6];
-        
+
         var tasks = new List<Task>();
-        foreach (var replica in ServerInfo.ServerRuntimeContext.Replicas.Where(x => x.Value.Connected))
+        var resp = RespBuilder.BuildRespArray("REPLCONF", "GETACK", "*");
+        
+        var connectedReplicas = ServerInfo.ServerRuntimeContext.Replicas
+            .Where(x => x.Value.Connected);
+        
+        foreach (var replica in connectedReplicas)
         {
             tasks.Add(Task.Run(() =>
-            {
-                replica.Value.Send("*3\r\n$8\r\nREPLCONF\r\n$6\r\nGETACK\r\n$1\r\n*\r\n"u8.ToArray());
-            }));
+                replica.Value.Send(resp.AsBytes())));
         }
 
         await Task.WhenAll(tasks);
 
         var sw = new Stopwatch();
         sw.Start();
-        
+
         while (sw.ElapsedMilliseconds < int.Parse(msToWait))
         {
             if (ServerInfo.Replication.ReplicaAcksReceived >= int.Parse(numberOfReplicasToWaitFor))
@@ -35,13 +39,13 @@ public class Wait : Base
                 break;
             }
         }
-        
+
         sw.Stop();
 
         var acksReceived = ServerInfo.Replication.ReplicaAcksReceived == 0
             ? ServerInfo.ServerRuntimeContext.GetConnectedReplicas()
             : ServerInfo.Replication.ReplicaAcksReceived;
-        
+
         var result = $":{acksReceived}\r\n";
         commandContext.Socket.Send(Encoding.UTF8.GetBytes(result));
         return result;
