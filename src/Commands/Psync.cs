d485f733ec1e571fa2dc4e25254ea30e63aa09d4
inner <simonas.pacauskas@gmail.com>
2024-11-16 15:08:32 +0200

    codecrafters submit [skip ci]

--- src/Commands/Psync.cs
+++ src/Commands/Psync.cs
@@ -1,5 +1,5 @@
-﻿using System.Text;
-using codecrafters_redis.Commands.Common;
+﻿using codecrafters_redis.Commands.Common;
+using codecrafters_redis.Common;
 
 namespace codecrafters_redis.Commands;
 
@@ -9,15 +9,14 @@ public class Psync : Base
 
     protected override Task<string> OnMasterNodeExecute(CommandContext commandContext)
     {
-        var fullResyncResponse = $"+FULLRESYNC {ServerInfo.ServerRuntimeContext.MasterReplId} 0\r\n";
+        var fullResync = RespBuilder.SimpleString($"FULLRESYNC {ServerInfo.ServerRuntimeContext.MasterReplId} 0");
+        commandContext.Socket.Send(fullResync.AsBytes());
         
-        var emptyRdbFileBase64 = "UkVESVMwMDEx+glyZWRpcy12ZXIFNy4yLjD6CnJlZGlzLWJpdHPAQPoFY3RpbWXCbQi8ZfoIdXNlZC1tZW3CsMQQAPoIYW9mLWJhc2XAAP/wbjv+wP9aog==";
-        var rdbFile = Convert.FromBase64String(emptyRdbFileBase64);
-        var rdbResynchronizationFileMsg = Encoding.UTF8.GetBytes($"${rdbFile.Length}\r\n")
+        var rdbFile = GetHardcodedEmptyRdbFile();
+        var rdbResynchronizationFileMsg = $"${rdbFile.Length}\r\n".AsBytes()
             .Concat(rdbFile)
             .ToArray();
         
-        commandContext.Socket.Send(Encoding.UTF8.GetBytes(fullResyncResponse));
         commandContext.Socket.Send(rdbResynchronizationFileMsg);
         
         ServerInfo.ServerRuntimeContext.Replicas.TryAdd(
@@ -25,5 +24,12 @@ public class Psync : Base
             commandContext.Socket);
         
         return Task.FromResult(string.Empty);
+    }
+    
+    private static byte[] GetHardcodedEmptyRdbFile()
+    {
+        return Convert.FromBase64String(
+            "UkVESVMwMDEx+glyZWRpcy12ZXIFNy4yLjD6CnJlZGlzLWJpdHPAQPoFY3Rpb" +
+            "WXCbQi8ZfoIdXNlZC1tZW3CsMQQAPoIYW9mLWJhc2XAAP/wbjv+wP9aog==");
     }
 }
