5cf17af8cb66b16de57522076ee2e0c9d987998a
Simonas <simonas.pacauskas@gmail.com>
2024-11-17 09:26:26 +0000

    codecrafters submit [skip ci]

--- src/Commands/Command.cs
+++ src/Commands/Command.cs
@@ -10,35 +10,76 @@ public class Command : Base
 
     protected override async Task<string> OnMasterNodeExecute(CommandContext commandContext)
     {
-        if (commandContext.CommandDetails.CommandParts.Length != 5 ||
-            commandContext.CommandDetails.CommandParts[4].ToUpper() != "DOCS")
+        string resp;
+
+        var commandPartsLength = commandContext.CommandDetails.CommandParts.Length;
+
+        if (commandPartsLength < 5 || !commandContext.CommandDetails.CommandParts[4]
+                .Equals("DOCS", StringComparison.CurrentCultureIgnoreCase))
         {
-            throw new ArgumentException("Invalid subcommand for COMMAND.");
+            resp = RespBuilder.Error("Invalid command");
+            commandContext.Socket.Send(resp.AsBytes());
+            return resp;
         }
 
-        var docs = await GetCommandDocs();
-        var result = RespBuilder.BulkString(docs);
-        commandContext.Socket.Send(result.AsBytes());
-        return result;
+        resp = await GetDocsResp(commandPartsLength == 7
+            ? commandContext.CommandDetails.CommandParts[6]
+            : null);
+
+        commandContext.Socket.Send(resp.AsBytes());
+        return resp;
     }
 
-    private Task<string> GetCommandDocs()
+    private static Task<string> GetDocsResp(string? commandKey = null)
     {
-        var commandDocs = new Dictionary<string, string>
+        var commands = new Dictionary<string, Dictionary<string, string>>
         {
-            { "GET", "GET <key>: Get the value of the specified key." },
-            { "SET", "SET <key> <value>: Set the value of the specified key." },
-            { "DEL", "DEL <key>: Delete the specified key." },
-            { "EXISTS", "EXISTS <key>: Determine if a key exists." },
-            { "COMMAND DOCS", "COMMAND DOCS: Returns documentation about commands." }
+            {
+                "GET",
+                new Dictionary<string, string>
+                {
+                    { "summary", "Returns the string value of a key." },
+                    { "since", "1.0.0" },
+                    { "group", "string" }
+                }
+            },
+            {
+                "SET",
+                new Dictionary<string, string>
+                {
+                    {
+                        "summary",
+                        "Sets the string value of a key, ignoring its type. The key is created if it doesn't exist."
+                    },
+                    { "since", "1.0.0" },
+                    { "group", "string" }
+                }
+            }
         };
 
-        var docs = new StringBuilder();
-        foreach (var entry in commandDocs)
+        var filteredCommands = commandKey == null
+            ? commands
+            : commands.Where(c => c.Key.Equals(commandKey, StringComparison.CurrentCultureIgnoreCase))
+                .ToDictionary(c => c.Key, c => c.Value);
+
+        if (filteredCommands.Count == 0)
         {
-            docs.AppendLine($"{entry.Key}: {entry.Value}");
+            return Task.FromResult(RespBuilder.EmptyArray());
         }
 
-        return Task.FromResult(docs.ToString());
+        var sb = new StringBuilder();
+        sb.Append($"*{filteredCommands.Count * 2}\r\n");
+        foreach (var command in filteredCommands)
+        {
+            sb.Append(RespBuilder.BulkString(command.Key));
+            sb.Append($"*{command.Value.Count * 2}\r\n");
+            foreach (var subCommand in command.Value)
+            {
+                sb.Append(RespBuilder.BulkString(subCommand.Key));
+                sb.Append(RespBuilder.BulkString(subCommand.Value));
+            }
+        }
+
+        return Task.FromResult(sb.ToString());
     }
 }
