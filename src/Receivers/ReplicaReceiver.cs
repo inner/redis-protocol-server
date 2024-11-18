a9438db3b49f1a28b2ba3e055109fa8d3bd97074
inner <simonas.pacauskas@gmail.com>
2024-11-18 14:27:13 +0200

    codecrafters submit [skip ci]

--- src/Receivers/ReplicaReceiver.cs
+++ src/Receivers/ReplicaReceiver.cs
@@ -1,6 +1,6 @@
 ﻿using System.Net.Sockets;
-using System.Text;
 using codecrafters_redis.Commands.Common;
+using codecrafters_redis.Common;
 
 namespace codecrafters_redis.Receivers;
 
@@ -8,10 +8,7 @@ public class ReplicaReceiver : ReceiverBase
 {
     public override async Task Receive(Socket socket, string commandString, List<CommandQueueItem> commandQueue)
     {
-        if (!ServerInfo.Replication.ReplicaHandshakeCompleted)
-        {
-            return;
-        }
+        if (!ServerInfo.Replication.ReplicaHandshakeCompleted) return;
         
         if (!ServerInfo.Replication.ReplicaFirstByteReceived)
         {
@@ -20,12 +17,7 @@ public class ReplicaReceiver : ReceiverBase
             return;
         }
 
-        if (commandString.EndsWith("\r\n\n"))
-        {
-            commandString = commandString[..^1];
-        }
-
-        var currentBytesReceived = Encoding.UTF8.GetBytes(commandString).Length;
+        var currentBytesReceived = commandString.AsBytes().Length;
         
         Console.WriteLine(
             $"Total bytes received: {ServerInfo.Replication.ReplicaBytesReceived}. " +
