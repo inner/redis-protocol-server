d55f29676e7bec6c1296ae67aba55d4d5cee27bb
inner <simonas.pacauskas@gmail.com>
2024-11-27 09:14:05 +0200

    codecrafters submit [skip ci]

--- Redis/Commands/Info.cs
+++ Redis/Commands/Info.cs
@@ -47,4 +47,30 @@ public class Info : Base
         
         return Task.FromResult(response);
     }
+
+    protected override Task<string> OnReplicaNodeExecute(CommandContext commandContext)
+    {
+        var infoValues = new Dictionary<string, string>
+        {
+            { "role", "slave" },
+            { "master_replid", string.Empty },
+            { "master_repl_offset", "0" },
+            { "connected_slaves", "0" },
+            { "second_repl_offset", "-1" },
+            { "repl_backlog_active", "0" },
+            { "repl_backlog_size", "1048576" },
+            { "repl_backlog_first_byte_offset", "0" },
+            { "repl_backlog_histlen", string.Empty }
+        };
+        
+        var infoValue = string.Join('\n', infoValues.Select(x => $"{x.Key}:{x.Value}"));
+        var response = RespBuilder.BulkString(infoValue);
+
+        if (!commandContext.ReplicaConnection)
+        {
+            commandContext.Socket.SendCommand(response);
+        }
+        
+        return Task.FromResult(response);
+    }
 }
