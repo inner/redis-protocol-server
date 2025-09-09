11b92ec6346021992357f2227aa7a7e6bfcce75b
Simonas <simonas.pacauskas@gmail.com>
2025-09-09 11:32:42 +0300

    codecrafters submit [skip ci]

--- Redis/Commands/Blpop.cs
+++ Redis/Commands/Blpop.cs
@@ -33,7 +33,7 @@ public class Blpop : Base
 
         if (result.Length == 0)
         {
-            resp = RespBuilder.Null();
+            resp = RespBuilder.EmptyArray();
             commandContext.Socket.SendCommand(resp);
 
             return resp;
