b01d47c4391b4f42fb32e5643689db6925993254
Simonas <simonas.pacauskas@gmail.com>
2025-09-11 17:35:26 +0300

    codecrafters submit [skip ci]

--- Redis/Nodes/NodeBase.cs
+++ Redis/Nodes/NodeBase.cs
@@ -43,10 +43,10 @@ public abstract class NodeBase(IPAddress localAddress, int port, ReceiverBase re
         }
     }
 
-    private void LogReceivedCommand(string resp)
+    private void LogReceivedCommand(string connection, string resp)
     {
         var logMessage = resp.Replace(Constants.NewLine, Constants.VerbatimNewLine);
-        Console.WriteLine($"[{NodeName}] Received command: [{logMessage}].");
+        Console.WriteLine($"[{NodeName}] [{connection}] Received command: [{logMessage}].");
     }
 
     protected async Task HandleConnection(TcpClient client)
@@ -74,7 +74,7 @@ public abstract class NodeBase(IPAddress localAddress, int port, ReceiverBase re
                     }
 
                     await receiver.Receive(client.Client, resp, commandQueue, subscriptions);
-                    LogReceivedCommand(resp);
+                    LogReceivedCommand(connectionId, resp);
                 }
             }
             catch (SocketException)
